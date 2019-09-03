28db0152fdd57daf04d5ddf521f21c8f5d8d3774
orpitadutta <orpitadutta8@gmail.com>
2019-09-03 19:07:43 +0530

    Get List

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-poc2demo';
+  
+  constructor (private httpService: HttpClient) { }
+  arrWells: string [];
+
+  ngOnInit () {
+    this.httpService.get('./assets/data.json').subscribe(
+      data => {
+        this.arrWells = data as string [];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.arrWells[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WellListComponent } from './well-list/well-list.component';
+import { WellComponent } from './well/well.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WellListComponent,
+    WellComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
